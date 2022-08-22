4685edb353e3e02f21b1079f0837e8908a6e270d
John Walsh <aurummail@gmail.com>
2022-08-23 00:24:00 +0300

    проработал задачи из семинара

--- lesson_1/ex_1/Program.cs
+++ lesson_1/ex_1/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.Write("input num ");
+// // //int x = int.Parse(Console.ReadLine());
+// int x = Convert.ToInt32(Console.ReadLine());
+// if (x == 0)
+// {
+//     Console.WriteLine("zero is zero FFS");
+// }
+// else {Console.WriteLine("square is " + x*x);}
+
+// Console.WriteLine("   SECOND ZADACHA   ");
+// Console.WriteLine("Please, input two numbers using enter-key:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a*a == b) {Console.WriteLine("yeah");}
+// else          {Console.WriteLine("nope");}
+
+// Console.WriteLine("   THIRD ZADACHA   ");
+// Console.Write("input num ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int i = -n;
+// while (i <=n)
+// {
+//     Console.Write(i + " ");
+//     i++;
+// }
+
+Console.WriteLine("   THIRD ZADACHA   ");
+Console.Write("input num ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = -n; i<=n; i++) Console.Write(i + " ");
+
+// Console.WriteLine("   FORTH ZADACHA   ");
+// Console.Write("input 3-digit num: ");
+// int z = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(z%10);
