8439856beeb69f2f41713e51e329b5f367dbed5d
John Walsh <aurummail@gmail.com>
2022-09-08 23:13:56 +0300

    решил проблему пустого ввода

---
+++ homework_6/Program.cs
@@ -0,0 +1,369 @@
+﻿/* 
+Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+вариация задачи 41 - пользователь не задает сколько
+чисел он введет (то есть число M), а подсчет количества
+чисел производится после того, как пользователь
+не ввел информацию и нажал Enter.
+
+Задача 43: Напишите программу, которая найдёт точку
+пересечения двух прямых, заданных уравнениями
+y = k1 * x + b1, y = k2 * x + b2; значения
+b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+задача 40 - HARD необязательная. На вход программы
+подаются три целых положительных числа. Определить,
+является ли это сторонами треугольника. Если да, то
+вывести всю информацию по нему - площадь, периметр,
+значения углов треугольника в градусах, является ли
+он прямоугольным, равнобедренным, равносторонним.
+
+задача 2 HARD необязательная. Сгенерировать массив
+случайных целых чисел размерностью m*n (размерность
+вводим с клавиатуры). Вывести на экран красивенько
+таблицей. Перемешать случайным образом элементы массива,
+причем чтобы каждый гарантированно переместился на другое
+место (возможно для этого удобно будет использование множества)
+и выполнить это за m*n / 2 итераций. То есть если массив
+три на четыре, то надо выполнить не более 6 итераций.
+И далее в конце опять вывести на экран как таблицу.
+ */
+
+// ЭТО СЕРВИСНЫЕ ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ
+// цель - сделат ь так, что бы вообще не трогать их
+// но пока не вижу, как грамотно осуществить селектор задач
+// их может быть и одна и сто одна..
+
+int InputChecker(string exeptionMessage) //немного изменённая стандартная функция для защиты от ввода не чисел
+                    // я понял , что текст ошибки может быть разным и надо его посылать в функцию
+                    // поэтому она принимаеит string аргумент
+{
+    try
+    {
+        return Convert.ToInt32(Console.ReadLine());  // почему то не работает без переноса строки -  return Convert.ToInt32(Console.Read());  
+    }
+    catch
+    {
+        Console.WriteLine(exeptionMessage);
+    }
+    return InputChecker(exeptionMessage);  
+}
+
+
+
+double InputDoubleChecker() // функция для защиты от ввода не-чисел
+                            // нам нужны однотипные проверки во всех задачах
+{
+    try
+    {
+        return Convert.ToDouble(Console.ReadLine()); 
+    }
+    catch
+    {
+        Console.Write("Работаем только числами, попробуй снова (ctrl+c для экстренного прерывания): ");
+    }
+    return InputChecker(); 
+}
+
+string EndingChanger(int num, string one, string two, string many) // Функция для замены окончания, на входе числительное и окончания
+                                                                   // для один -ист, два -иста, много -истов, окончания для разных
+                                                                   // слов могут быть разные, поэтому посылаем их в функцию
+                                                                   // и весь этот код только для изменения окончания в одном месте,
+                                                                   // который почти никогда не будет меняться, всегда по 4 задачи...
+{
+    int n = num % 100;
+    string ending = string.Empty; // это будем посылать в функцию для установки окончания
+
+    if (n / 10 == 1) ending = many;                        // предпоследний разряд - 1
+    else  // в этом ветвлении проверяем последний разряд
+    {
+        if (n % 10 == 1) ending = one;                     // последний разряд - 1
+        else if (n % 10 >= 2 && n % 10 <= 4) ending = two; // последний разряд - 2..4
+        else ending = many;                                // последний разряд - 5..9 или 0
+    }
+    return ending;
+}
+
+bool ExInit(int exerciseNumber, int totalCount, string[] exerciseText) // выбор и инициализация конкретной задачи, 
+                                                                       // после выполнения - возвращаем значение флага,
+                                                                       // который показывает будем дальше работать
+                                                                       // или же прерываем работу
+                                                                       // exerciseNumber - номер задачи
+                                                                       // totalCount - количество задач
+{
+    if (exerciseNumber > 0 && exerciseNumber <= totalCount)
+    {
+        Console.WriteLine($"\n---Exercise {exerciseNumber}---");
+        Console.WriteLine(exerciseText[exerciseNumber - 1]);
+        switch (exerciseNumber)
+        {
+            case 1: { ExOne(); break; }
+            case 2: { ExTwo(); break; }
+            case 3: { ExThree(); break; }
+            case 4: { ExFour(); break; }
+           // case 5: { ExFive(); break; }
+        }
+        Console.WriteLine("---End of Exercise---");
+        return true;
+    }
+    else
+    {
+        switch (exerciseNumber)
+        {
+            case 0: return false;
+            default: { Console.WriteLine($"Здесь только {totalCount} задач{EndingChanger(totalCount, "а", "и", "")}..."); return true; }
+        }
+    }
+}
+
+int[] CreateArray(int n, int min, int max) // создание массива размера n и заполнение целыми числами из диапазона min..max
+{
+    if (max < min)  // проверка и защита
+    {
+        int temp = min;
+        min = max;
+        max = temp;
+    }
+
+    var rand = new Random();
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++) array[i] = rand.Next(min, max+1); // заполнение массива, не забывая, что справа плюс 1 к диапазону
+    return array;
+}
+
+double[] CreateDoubleArray(int n, double min, double max) // создание массива размера n и заполнение целыми числами из диапазона min..max
+{
+    if (max < min)  // проверка и защита
+    {
+        double temp = min;
+        min = max;
+        max = temp;
+    }
+
+    var rand = new Random();
+    double[] array = new double[n];
+    for (int i = 0; i < n; i++) array[i] = min + (max-min)*rand.NextDouble(); // заполнение массива
+                                                                              // rand.NextDouble() возвращает вещественное число из диапазона от 0 до 1
+                                                                              // дальше, что бы получить нужный нам диапазон мы должны начать со значения
+                                                                              // min и добавить к нему случайное число размером не больше , чем (max-min)
+    return array;
+}
+
+// посмотреть, можно ли в C# как-то организовать перегрузку метода по типу возвращаемого значения
+
+// ЗАДАЧИ
+
+void ExOne()
+{
+ 
+    Console.Write("Введите первое число число: ");
+
+    int [][] megaArray = new int [30][]; //примерно 4 млрд чисел, думаю, хватит. Однако хз, как сделать потенциально бесконечный ввод,
+    megaArray [0] = new int [8]; //начнём с 8-ми элементов и будем удваивать при заполнении. 
+
+    string nextInput = string.Empty,
+           exeptionMessage = "!Работаем только с целыми числами, введите или число или пустую строку для завершения ввода (ctrl+c для экстренного прерывания)!";
+
+    int nextNumber = 0, 
+        counter = 0, 
+        currentIndex = 0, 
+        currentMaxIndex = megaArray[currentIndex].Length;
+
+    while (true) 
+    {        
+        nextInput = Console.ReadLine();
+        if (nextInput == "") 
+        {
+            Console.WriteLine("Вы завершили ввод чисел.");
+            break;
+        }
+        else
+        {          
+            try 
+            {
+                nextNumber = Convert.ToInt32(nextInput);
+                tempArray[currentIndex] = Convert.ToInt32(nextInput);
+                currentIndex++;
+            }
+            catch
+            {
+                Console.WriteLine(exeptionMessage);
+            }
+        }     
+        Console.Write("Введите следующее число: ");           
+    } 
+
+
+
+
+
+
+/*     int n = 0;
+    while (true) // защита от ввода отрицательной размерности массива
+    {
+        Console.Write("Задайте размерность массива: ");
+        n = InputChecker();
+        if (n < 0) Console.WriteLine("Так не получится... размерность массива должна быть натуральным числом... ");
+        else break;
+    }
+    int[] a = CreateArray(n, 100, 1000); // создаём массив и выводим его на экран
+    string list = string.Join(", ", a);
+    
+    Console.WriteLine($"Созданный массив:\n[{list}]"); 
+
+    int count = 0;
+    for (int i = 0; i < a.Length; i++)
+        if (a[i]%2 == 0) count++;
+    
+     Console.WriteLine($"В этом массиве {count}"                            // вывод количества чётных чисел
+                     + $" чётн{EndingChanger(count, "ое", "ых", "ых")}"     // ееее! используем окончания))
+                     + $" чис{EndingChanger(count, "ло", "ла", "ел")}");  */
+}
+
+void DynamicArray ()
+{
+
+}
+
+void ExTwo()
+{
+    int n = 0, min=0, max =0;
+    while (true) // защита от ввода отрицательной размерности массива
+    {
+        Console.Write("Задайте размерность массива: ");
+        n = InputChecker();
+        if (n < 0) Console.WriteLine("Так не получится... размерность массива должна быть натуральным числом... ");
+        else break;
+    }
+
+    Console.Write("Введите диапазон значений элементов массива\nМинимальное значение: ");
+    min = InputChecker();
+    Console.Write("Максимальное значение: ");
+    max = InputChecker();
+
+    int[] a = CreateArray(n, min, max); // создаём массив и выводим его на экран
+    string list = string.Join(", ", a);
+    Console.WriteLine($"Созданный массив:\n[{list}]");
+
+    int sum = 0;
+    for (int i = 1; i < a.Length; i+=2) sum+=a[i];
+    
+     Console.WriteLine($"Сумма элементов этого массива, стоящих на нечётных позициях равна {sum}"); // вывод суммы
+}
+
+void ExThree()
+{
+    int n = 0;
+    double min=0, max =0;
+    while (true) // защита от ввода отрицательной размерности массива
+    {
+        Console.Write("Задайте размерность массива: ");
+        n = InputChecker();
+        if (n < 0) Console.WriteLine("Так не получится... размерность массива должна быть натуральным числом... ");
+        else break;
+    }
+
+    Console.Write("Введите диапазон вещестенных значений элементов массива\nМинимальное значение: ");
+    min = InputDoubleChecker();
+    Console.Write("Максимальное значение: ");
+    max = InputDoubleChecker();
+
+    double[] a = CreateDoubleArray(n, min, max); // создаём массив и выводим его на экран
+    string list = string.Join(", ", a);
+    Console.WriteLine($"Созданный массив:\n[{list}]");
+
+    double dif = 0, minNum = a[0], maxNum = a[0];
+    for (int i = 1; i < a.Length; i++)
+    {
+        if (a[i] < minNum) minNum = a[i];
+        else if (a[i] > maxNum) maxNum = a[i];
+    }
+
+    dif = maxNum - minNum;
+    Console.WriteLine($"Максимальный элемент массива: {maxNum}");
+    Console.WriteLine($"Минимальный элемент массива: {minNum}");
+    Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна {dif}"); // вывод разницы
+
+}
+
+void ExFour()
+{
+    Console.Write($"Введите число для проверки на палиндромность: ");
+    int n = InputChecker();   // думаю, что, по-хорошему надо вводить через строку, и если на первых позицях будут стоять нули - тогда мы их не будем терять
+    if (n < 1) n *= -1;  // убираем минус , если будет
+    if (PalindromChecker(n)) Console.WriteLine("Сие число есмь палиндромъ!");
+    else Console.WriteLine("Число сие палиндромомъ никакъ не является!");
+}
+
+bool PalindromChecker(int n) 
+{
+    int count = 0; // вспомогательнгый счётчик для поиска общего количества знаков
+    for (int i = 1; i <= n; i *= 10) count++;
+
+     int leftDigit = 0, rightDigit =0;
+    for (int i = 1; i <= count/2 + count%2; i++) // если число чётное, то проверок ровно половину от количества знаков, если нечётное - то надо исключить центральную цифру, а потом так же половина
+    {
+        //последовательно отсекаем части числа , что бы получить по цифре для сравнения
+   
+        leftDigit = n / IntPower(10, count-i); // Console.WriteLine("left dig before " + leftDigit + " "); // отладка
+        leftDigit %= IntPower(10, 1);          // Console.WriteLine("left dig after " + leftDigit + " ");
+
+        rightDigit = n % IntPower(10, i);      // Console.WriteLine("right dig before " +rightDigit + " ");
+        rightDigit /= IntPower(10, i-1);       // Console.WriteLine("right dig after " +rightDigit + " "); 
+
+        if (leftDigit != rightDigit) return false;         
+    }
+    return true;
+}
+
+int IntPower(int number, int power) // integer функция возведения в степень 
+{
+    if (power == 0) return 1;
+    int result = number;
+    for (int i = 1; i < power; i++) result *= number;
+    // Console.Write("power is " + result + ", "); // отладка
+    return result;
+}
+
+double VectorLength(int[] a, int[] b) // длина отрезка между двумя заданными точками в N-мерном пространстве
+{
+    int sum = 0;
+    int length = a.Length;
+    for (int i = 0; i < length; i++) sum += (a[i] - b[i]) * (a[i] - b[i]);
+    return Math.Sqrt(sum);
+}
+
+
+
+// ГЛАВНЫЙ КОД НАЧИНАЕТСЯ ЗДЕСЬ
+
+
+Console.WriteLine("\n    --- Домашка шестая ---");
+bool flag = true;
+int num = 4; // придётся руками менять значение num и ветвление в функции ExInit()
+
+string[] exerciseList = {
+                            "1 задача: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. При этом пользователь не задает сколько чисел он введет (то есть число M), а подсчет количества чисел производится после того, как пользователь не ввел информацию и нажал Enter.",
+                            "2 задача: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.",
+                            "3 задача HARD: На вход программы подаются три целых положительных числа. Определить , является ли это сторонами треугольника. Если да, то вывести всю информацию по нему - площадь, периметр, значения углов треугольника в градусах, является ли он прямоугольным, равнобедренным, равносторонним.",
+                            "4 задача HARD: Сгенерировать массив случайных целых чисел размерностью m*n (размерность вводим с клавиатуры). Вывести на экран красивенько таблицей. Перемешать случайным образом элементы массива, причем чтобы каждый гарантированно переместился на другое место (возможно для этого удобно будет использование множества) и выполнить это за m*n / 2 итераций. То есть если массив три на четыре, то надо выполнить не более 6 итераций. И далее в конце опять вывести на экран как таблицу."
+                        };
+
+foreach (string text in exerciseList) Console.WriteLine(text);
+
+while (flag) // бесконечный цикл, пока флаг = true. 
+{
+    Console.Write($"\nВведите или номер задачи (1..{num}) или 0 "
+                    + "для выхода (для экстренного прерывания - ctrl+C): ");
+
+    flag = ExInit(InputChecker(), num, exerciseList);
+
+    if (!flag)
+    {
+        Console.WriteLine("---Good buy!---\n");
+        break;
+    }
+}
---
+++ lesson_1/ex_6/Program.cs
@@ -0,0 +1,258 @@
+﻿void PrintArray (int[] arr)
+{
+    string list = string.Join(", ", arr);
+    Console.WriteLine($"Массив:\n[{list}]"); 
+}
+
+int InputChecker() // функция для защиты от ввода не-целых-чисел
+                   // нам нужны однотипные проверки во всех задачах
+{
+    try
+    {
+        return Convert.ToInt32(Console.ReadLine());  // почему то не работает без переноса строки -  return Convert.ToInt32(Console.Read());  
+    }
+    catch
+    {
+        Console.Write("Работаем только с целыми числами, попробуй снова (ctrl+c для экстренного прерывания): ");
+    }
+    return InputChecker();  
+}
+
+int[] CreateArray(int n, int min, int max) // создание массива размера n и заполнение целыми числами из диапазона min..max
+{
+    if (max < min)  // проверка и защита
+    {
+        int temp = min;
+        min = max;
+        max = temp;
+    }
+
+    var rand = new Random();
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++) array[i] = rand.Next(min, max+1); // заполнение массива, не забывая, что справа плюс 1 к диапазону
+    return array;
+}
+
+// Console.Write("Введите размерность массива: ");
+// int n = InputChecker();
+
+// int [] numbers = new int[n];
+// for(int i = 0; i < n; i++)
+// {
+//     numbers[i] = new Random().Next(0,100);
+//     if (i!=n-1)  Console.Write($"{numbers[i]}, ");
+//     else Console.WriteLine($"{numbers[i]}");
+// }
+// Console.WriteLine();
+
+// int buf;
+
+// for (int i = 0; i < n/2; i++)
+// {
+//     buf = numbers[i];
+//     numbers[i] = numbers[n-1-i];
+//     numbers[n-1-i] = buf;
+    
+// }
+
+// PrintArray(numbers);
+
+
+/* Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10 */
+
+/* Console.WriteLine("Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины."
+                +" Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.");
+bool TriChecker (int a, int b, int c)
+{
+    if (a+b>c && b+c>a && c+a>b) return true;
+    else return false;
+}
+
+Console.Write("Введите сторону: ");
+int a = InputChecker();
+Console.Write("Введите сторону: ");
+int b = InputChecker();
+Console.Write("Введите сторону: ");
+int c = InputChecker();
+
+if (TriChecker(a,b,c)) Console.WriteLine("да, треугло");
+else Console.WriteLine("нет, не треугло");
+
+
+Console.Write("Введите десятичное число: ");
+int d = InputChecker(), dv = 0;
+
+*/
+/*  //чужое
+int[] RandomArray(int size, int minValue, int maxValue) {
+    int[] result = new int[size];
+    for (int i = 0; i < size; ++i) {
+        result[i] = new Random().Next(minValue, maxValue);
+    }
+    return result;
+} 
+ */
+
+/* //чужое
+ void PrintArray(int[] array) {
+    for(int i = 0; i < array.Length - 1; ++i) {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine(array[array.Length - 1]);
+} */
+
+void Reverse(int[] array) {
+    for (int i = 0; i < array.Length / 2; ++i) {
+        int temp = array[i];
+        array[i] = array[array.Length - 1 -i];
+        array[array.Length - 1 -i] = temp;
+    }
+}
+/*  //чужое
+void Task1() {
+    int[] arr = RandomArray(6, 1, 100);
+    PrintArray(arr);
+    Reverse(arr);
+    PrintArray(arr);
+}
+
+bool IsTrianglePossible(int a, int b, int c) {
+    return (a < b + c && b < a + c && c < a + b);
+} */
+/* 
+void Task40() {
+    Console.Write("Введите первую сторону треугольника: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите вторую сторону треугольника: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите третью сторону треугольника: ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"{a}, {b}, {c} - треугольник ");
+    if (!IsTrianglePossible(a, b, c)) {
+        Console.Write("не ");
+    }
+    Console.WriteLine("возможен.");
+}
+
+int ConvertToBinary(int number) {
+    int result = 0;
+    int order = 1;                      // Переменная для перехода на следующую цифру числа в двоичной записи
+    while (number > 0) {                
+        result += (number % 2) * order; // Записываем в результирующую переменную следующую цифру 
+                                        // (Каждая цифра двоичной записи - остаток от деления на 2,
+                                        // умножение на order позволяет записать следующую цифру отдельно,
+                                        // вместо того, чтобы прибавлять к первому числу)
+        order *= 10;
+        number /= 2;
+    }
+    return result;
+}
+
+
+
+void Task42() {
+    Console.Write("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{x} -> {ConvertToBinary(x)}");
+}
+
+//Task40();
+Task42();
+ */
+
+
+/* Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int c = 0;
+string b = String.Empty;
+while (a >= 1)
+{
+    c = a%2;
+    b = b+Convert.ToString(c);
+    a = a/2;
+}
+char [] b2 = b.ToCharArray();
+Array.Reverse(b2);
+Console.WriteLine(b2);
+ */
+
+/* 
+ Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+ Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+ Задача 45: Напишите программу, которая будет создавать копию 
+ заданного массива с помощью поэлементного копирования. */
+
+
+
+
+Console.Write("Введите количество чисел, необходимых для вывода: ");
+int n = InputChecker();
+int a = 0, b =1, c =0;
+
+if (n==0) Console.Write(a);
+else if (n == 1) Console.Write(a + " " + b);
+else 
+{
+    Console.Write(a + " " + b + " ");
+    for (int i = 2; i < n; i++)
+    {
+        c = a+b;
+        Console.Write(c + " ");
+        a =b;
+        b =c;
+    }
+}
+Console.WriteLine();
+
+/* 
+Console.Write("Введите количество чисел, необходимых для вывода: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int x = 0, y =1, z =0;
+
+if (n==0) Console.Write(a);
+else if (n == 1) Console.Write(a + " " + b);
+else 
+{
+    Console.Write(a + " " + b + " ");
+    for (int i = 2; i < n; i++)
+    {
+        c = a+b;
+        Console.Write(c + " ");
+        a =b;
+        b =c;
+    }
+} */
+
+
+
+    while (true) // защита от ввода отрицательной размерности массива
+    {
+        Console.Write("Задайте размерность массива: ");
+        n = InputChecker();
+        if (n < 0) Console.WriteLine("Так не получится... размерность массива должна быть натуральным числом... ");
+        else break;
+    }
+    Console.Write("Введите диапазон значений элементов массива\nМинимальное значение: ");
+    int min = InputChecker();
+    Console.Write("Максимальное значение: ");
+    int max = InputChecker();
+    int[] x = CreateArray(n, min, max); // создаём массив и выводим его на экран
+    string list = string.Join(", ", x);    
+    Console.WriteLine($"Созданный массив:\n[{list}]"); 
+
+    int[] y = CreateArray(n, 0, 0); 
+
+    for (int i = 0; i < x.Length; i++) y[i] = x[i];
+
+    list = string.Join(", ", y);    
+    Console.WriteLine($"Созданный массив:\n[{list}]"); 
+
